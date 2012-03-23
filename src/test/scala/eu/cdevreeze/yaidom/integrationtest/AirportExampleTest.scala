66e619c671603b2d09566b956b047f3e2ae2548f
Chris de Vreeze <dvreeze@xs4all.nl>
2012-03-23 01:22:53 +0100

    Working on the new test case

--- src/test/scala/eu/cdevreeze/yaidom/integrationtest/AirportExampleTest.scala
+++ src/test/scala/eu/cdevreeze/yaidom/integrationtest/AirportExampleTest.scala
@@ -18,13 +18,15 @@ package eu.cdevreeze.yaidom
 package integrationtest
 
 import java.{ util => jutil, io => jio }
+import javax.xml.parsers._
+import javax.xml.transform.TransformerFactory
 import scala.collection.immutable
 import org.junit.{ Test, Before, Ignore }
 import org.junit.runner.RunWith
 import org.scalatest.{ Suite, BeforeAndAfterAll }
 import org.scalatest.junit.JUnitRunner
-import parse.DocumentParserUsingSax
-import print.DocumentPrinterUsingSax
+import parse._
+import print._
 
 /**
  * Test case using yaidom on files of airports.
@@ -67,6 +69,222 @@ class AirportExampleTest extends Suite {
     val rootElms = List(deAirportsDoc, beAirportsDoc, nlAirportsDoc) map { doc => doc.documentElement }
 
     rootElms foreach { root => validateDocumentStructure(root) }
+  }
+
+  @Test def testShortDocumentQueries() {
+    // 1. Parse XML files into Documents
+
+    val domParser = DocumentParserUsingDom.newInstance
+
+    val deAirportsDoc: Document = {
+      val is = classOf[AirportExampleTest].getResourceAsStream("airportsGermany.xml")
+      domParser.parse(is)
+    }
+
+    val beAirportsDoc: Document = {
+      val is = classOf[AirportExampleTest].getResourceAsStream("airportsBelgium.xml")
+      domParser.parse(is)
+    }
+
+    val nlAirportsDoc: Document = {
+      val is = classOf[AirportExampleTest].getResourceAsStream("airportsNetherlands.xml")
+      domParser.parse(is)
+    }
+
+    // 2. Sanity checks on the documents
+
+    val beAirportCountries = {
+      val result = beAirportsDoc.documentElement collectFromElems { case e if e.qname.localPart == "Country" => e.trimmedText }
+      result.toSet
+    }
+    expect(Set("Belgium")) {
+      beAirportCountries
+    }
+
+    val nlAirportCountryCodes = {
+      val result = nlAirportsDoc.documentElement collectFromElems { case e if e.qname.localPart == "CountryCode" => e.trimmedText }
+      result.toSet
+    }
+    expect(Set("461")) {
+      nlAirportCountryCodes
+    }
+
+    val deAirportCountryAbbrevs = {
+      val result = deAirportsDoc.documentElement collectFromElems { case e if e.qname.localPart == "CountryAbbrviation" => e.trimmedText }
+      result.toSet
+    }
+    expect(Set("DE")) {
+      deAirportCountryAbbrevs
+    }
+
+    // 3. Check elevation query results
+
+    val enameTable = nsWebServiceX.ename("Table")
+    val enameRunwayElevationFeet = nsWebServiceX.ename("RunwayElevationFeet")
+
+    def highestAirport(root: Elem): Elem = {
+      val sorted = root.elemsOrSelf(enameTable) sortBy { e =>
+        e.childElemOption(enameRunwayElevationFeet) map { e => e.trimmedText.toInt } getOrElse (0)
+      }
+      sorted.last
+    }
+
+    val highestNlAirport = highestAirport(nlAirportsDoc.documentElement)
+    val highestBeAirport = highestAirport(beAirportsDoc.documentElement)
+    val highestDeAirport = highestAirport(deAirportsDoc.documentElement)
+
+    def airportElevationInFeet(e: Elem): Int = {
+      require(e.resolvedName == enameTable)
+      e.childElem(enameRunwayElevationFeet).trimmedText.toInt
+    }
+
+    // The highest German airport is Oberpfaffenhofen (Munich)
+    expect("OBF") {
+      airportCode(highestDeAirport)
+    }
+    assert(airportElevationInFeet(highestDeAirport) > 1800)
+    assert(airportElevationInFeet(highestDeAirport) < 2000)
+
+    // The highest Belgian airport is Liege
+    expect("LGG") {
+      airportCode(highestBeAirport)
+    }
+
+    // The highest Dutch airport is Maastricht
+    expect("MST") {
+      airportCode(highestNlAirport)
+    }
+
+    // Checking some obvious facts:
+    // The highest airport in Germany is higher than the highest in Belgium, which is higher than the highest in the Netherlands.
+    // The highest of these 3 is also the one located south of the next highest, and so on.
+
+    assert(airportElevationInFeet(highestDeAirport) > airportElevationInFeet(highestBeAirport))
+    assert(airportElevationInFeet(highestBeAirport) > airportElevationInFeet(highestNlAirport))
+
+    assert(airportLatitude(highestDeAirport) < airportLatitude(highestBeAirport))
+    assert(airportLatitude(highestBeAirport) < airportLatitude(highestNlAirport))
+
+    // 4. Check distance query results
+
+    // Airports close enough to Arnhem (not taking the route into account)
+
+    val latLonArnhem = LatLon(52.0, 5.9)
+
+    val maxDistance = 160.0
+
+    val nlAirportsCloseToArnhem = {
+      val airportElms = nlAirportsDoc.documentElement.allChildElems
+
+      airportElms filter { e =>
+        val latLon = LatLon(airportLatitude(e), airportLongitude(e))
+        latLon.distance(latLonArnhem) <= maxDistance
+      }
+    }
+
+    val deAirportsCloseToArnhem = {
+      val airportElms = deAirportsDoc.documentElement.allChildElems
+
+      airportElms filter { e =>
+        val latLon = LatLon(airportLatitude(e), airportLongitude(e))
+        latLon.distance(latLonArnhem) <= maxDistance
+      }
+    }
+
+    val nlAirportCodesCloseToArnhem = {
+      val result = nlAirportsCloseToArnhem map { e => airportCode(e) }
+      result.toSet
+    }
+    val deAirportCodesCloseToArnhem = {
+      val result = deAirportsCloseToArnhem map { e => airportCode(e) }
+      result.toSet
+    }
+
+    // Schiphol, Rotterdam, Eindhoven and Maastricht are close enough
+    assert(Set("AMS", "RTM", "EIN", "MST").subsetOf(nlAirportCodesCloseToArnhem))
+
+    // Duesseldorf and Koeln-Bonn are close enough
+    assert(Set("DUS", "CGN").subsetOf(deAirportCodesCloseToArnhem))
+    // Frankfurt is quite a bit further away
+    assert(!deAirportCodesCloseToArnhem.contains("FRA"))
+  }
+
+  @Test def testDocumentTransformations() {
+    // 1. Parse XML files into Documents
+
+    val staxParser = DocumentParserUsingStax.newInstance
+
+    val deAirportsDoc: Document = {
+      val is = classOf[AirportExampleTest].getResourceAsStream("airportsGermany.xml")
+      staxParser.parse(is)
+    }
+
+    val beAirportsDoc: Document = {
+      val is = classOf[AirportExampleTest].getResourceAsStream("airportsBelgium.xml")
+      staxParser.parse(is)
+    }
+
+    val nlAirportsDoc: Document = {
+      val is = classOf[AirportExampleTest].getResourceAsStream("airportsNetherlands.xml")
+      staxParser.parse(is)
+    }
+
+    // 2. Transform the XML. We do it in small steps, but still using for-comprehensions.
+
+    val scope = Scope.fromMap(Map("" -> "http://www.webserviceX.NET"))
+
+    val airportRootElm =
+      Elem(
+        qname = "NewDataSet".qname,
+        scope = scope,
+        children = deAirportsDoc.documentElement.allChildElems ++ beAirportsDoc.documentElement.allChildElems ++ nlAirportsDoc.documentElement.allChildElems)
+
+    expect(deAirportsDoc.documentElement.allChildElems.size + beAirportsDoc.documentElement.allChildElems.size + nlAirportsDoc.documentElement.allChildElems.size) {
+      airportRootElm.allChildElems.size
+    }
+
+    // The airport codes we are now interested in
+    val airportCodes = List("BRU", "AMS", "HAM", "FRA", "CGN", "MUC", "TXL")
+
+    val airportElms: immutable.Seq[Elem] = {
+      // Contains duplicates
+      val elms = airportRootElm childElemsWhere { e => airportCodes.contains(airportCode(e)) }
+      val groups = elms groupBy { e => airportCode(e) }
+      // No more duplicates
+      val result = groups.values map { grp => grp.head }
+      result.toList sortBy { e => airportCodes.indexOf(airportCode(e)) }
+    }
+
+    import NodeBuilder._
+
+    // TODO Complete the summary, and make some assertions about it
+
+    val airportSummaryElms: immutable.Seq[Elem] =
+      for {
+        airportElm <- airportElms
+      } yield {
+        val elmBuilder: ElemBuilder =
+          elem(
+            qname = "Airport".qname,
+            children = immutable.IndexedSeq(
+              elem(
+                qname = "AirportCode".qname,
+                children = immutable.IndexedSeq(text(airportCode(airportElm))))))
+
+        elmBuilder.build(scope)
+      }
+
+    val airportSummaryRoot = Elem(qname = "Airports".qname, scope = scope, children = airportSummaryElms.toIndexedSeq)
+
+    val domPrinter = {
+      val dbf = DocumentBuilderFactory.newInstance
+      val tf = TransformerFactory.newInstance
+      tf.setAttribute("indent-number", java.lang.Integer.valueOf(2))
+      DocumentPrinterUsingDom.newInstance(dbf, tf)
+    }
+
+    val summaryXmlString = domPrinter.print(Document(airportSummaryRoot))
+    println(summaryXmlString)
   }
 
   private def validateDocumentStructure(root: Elem) {
@@ -116,6 +334,66 @@ class AirportExampleTest extends Suite {
     expect(0) {
       val elms = tablePropertyElms flatMap { _.allChildElems }
       elms.size
+    }
+  }
+
+  private def airportLatitude(e: Elem): Double = {
+    require(e.resolvedName == nsWebServiceX.ename("Table"))
+
+    val degree = e.childElem(nsWebServiceX.ename("LatitudeDegree")).trimmedText.toDouble
+    val minute = e.childElem(nsWebServiceX.ename("LatitudeMinute")).trimmedText.toDouble
+    val second = e.childElem(nsWebServiceX.ename("LatitudeSecond")).trimmedText.toDouble
+
+    val north = {
+      val result = e.childElem(nsWebServiceX.ename("LatitudeNpeerS")).trimmedText
+      (result != "S")
+    }
+
+    val absoluteValue = degree + (minute / 60) + (second / 3600)
+    if (north) absoluteValue else -absoluteValue
+  }
+
+  private def airportLongitude(e: Elem): Double = {
+    require(e.resolvedName == nsWebServiceX.ename("Table"))
+
+    val degree = e.childElem(nsWebServiceX.ename("LongitudeDegree")).trimmedText.toDouble
+    val minute = e.childElem(nsWebServiceX.ename("LongitudeMinute")).trimmedText.toDouble
+    val second = e.childElem(nsWebServiceX.ename("LongitudeSeconds")).trimmedText.toDouble
+
+    val east = {
+      val result = e.childElem(nsWebServiceX.ename("LongitudeEperW")).trimmedText
+      (result != "W")
+    }
+
+    val absoluteValue = degree + (minute / 60) + (second / 3600)
+    if (east) absoluteValue else -absoluteValue
+  }
+
+  private def airportCode(e: Elem): String = {
+    require(e.resolvedName == nsWebServiceX.ename("Table"))
+
+    e.childElem(nsWebServiceX.ename("AirportCode")).trimmedText
+  }
+
+  final case class LatLon(val lat: Double, val lon: Double) {
+    import scala.math._
+
+    /** Returns the distance to another LatLon. See http://en.wikipedia.org/wiki/Haversine_formula. */
+    def distance(other: LatLon): Double = {
+      val radiusEarth = 6371.0
+
+      val dLat = toRadians(other.lat - lat)
+      val dLon = toRadians(other.lon - lon)
+
+      val lat1 = toRadians(lat)
+      val lat2 = toRadians(other.lat)
+
+      val a = pow((dLat / 2), 2) + (cos(lat1) * cos(lat2) * pow(sin(dLon / 2), 2))
+
+      val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+      val d = radiusEarth * c
+      d
     }
   }
 }
