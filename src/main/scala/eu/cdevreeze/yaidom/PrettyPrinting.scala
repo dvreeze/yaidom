734edb954da202315bdcc3da6a65f86fc873cb73
Chris de Vreeze <dvreeze@xs4all.nl>
2012-07-05 21:37:30 +0200

    Added @author and license

--- src/main/scala/eu/cdevreeze/yaidom/PrettyPrinting.scala
+++ src/main/scala/eu/cdevreeze/yaidom/PrettyPrinting.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package eu.cdevreeze.yaidom
 
 import scala.collection.{ immutable, mutable }
@@ -10,6 +26,8 @@ import org.apache.commons.lang3.StringEscapeUtils
  * This API is safe to use, because of the use of "immutability everywhere". On the down-side, this very likely negatively
  * affects performance. On the other hand, the design is such that repeated nested indentation (shifting) does not cause
  * any string concatenation, until the lines are all materialized.
+ *
+ * @author Chris de Vreeze
  */
 private[yaidom] object PrettyPrinting {
 
