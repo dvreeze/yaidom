c294551a6ae60ff26546c60b0f4d54a733eda064
Chris de Vreeze <dvreeze@xs4all.nl>
2012-11-13 22:09:57 +0100

    Another Groovy XML example written in Scala with yaidom

---
+++ src/test/scala/eu/cdevreeze/yaidom/integrationtest/XmlToFlatFileTest.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2011 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.yaidom
+package integrationtest
+
+import java.{ util => jutil, io => jio }
+import scala.collection.immutable
+import org.junit.{ Test, Before, Ignore }
+import org.junit.runner.RunWith
+import org.scalatest.{ Suite, BeforeAndAfterAll }
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * Test case that "ports" http://www.jroller.com/jurberg/entry/converting_xml_to_flat_files to Scala.
+ * Here we do not need any dynamic language with lambdas. A static language with lambdas, such as Scala, will do just fine.
+ *
+ * @author Chris de Vreeze
+ */
+@RunWith(classOf[JUnitRunner])
+class XmlToFlatFileTest extends Suite {
+
+  @Test def testConvertXmlToFlatFile() {
+    val docParser = parse.DocumentParserUsingStax.newInstance
+
+    val xmlData = """<?xml version="1.0"?>
+<catalog>
+   <book id="bk101">
+      <author>Gambardella, Matthew</author>
+      <title>XML Developer's Guide</title>
+      <genre>Computer</genre>
+      <price>44.95</price>
+      <publish_date>2000-10-01</publish_date>
+   </book>
+   <book id="bk102">
+      <author>Ralls, Kim</author>
+      <title>Midnight Rain</title>
+      <genre>Fantasy</genre>
+      <price>5.95</price>
+      <publish_date>2000-12-16</publish_date>
+   </book>
+   <book id="bk103">
+      <author>Corets, Eva</author>
+      <title>Maeve Ascendant</title>
+      <genre>Fantasy</genre>
+      <price>5.95</price>
+      <publish_date>2000-11-17</publish_date>
+   </book>
+</catalog>
+      """
+
+    val rootElem = docParser.parse(new jio.ByteArrayInputStream(xmlData.getBytes("utf-8"))).documentElement
+
+    val columnMapping = Vector(
+      { bookElem: Elem =>
+        val id = (bookElem \@ "id").mkString.trim
+        id.padTo(5, ' ').take(5)
+      },
+      { bookElem: Elem =>
+        val lastName = (bookElem \ "author").map(_.text).mkString.split(',').apply(0).trim
+        lastName.padTo(15, ' ').take(15)
+      },
+      { bookElem: Elem =>
+        val firstName = (bookElem \ "author").map(_.text).mkString.split(',').apply(1).trim
+        firstName.padTo(10, ' ').take(10)
+      })
+
+    val separator = System.getProperty("line.separator")
+
+    val rows = (rootElem \ "book") map { e =>
+      val columns = (0 until 3).map(idx => columnMapping(idx)(e)).mkString
+      columns + separator
+    }
+
+    val expected =
+      "bk101Gambardella    Matthew   " + separator +
+        "bk102Ralls          Kim       " + separator +
+        "bk103Corets         Eva       " + separator
+
+    expect(expected) {
+      rows.mkString
+    }
+  }
+}
