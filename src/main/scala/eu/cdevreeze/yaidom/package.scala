bc7aa34aeea101daba49967521d50a2cd7ef4fb2
Chris de Vreeze <chris.de.vreeze@caiway.net>
2013-09-04 20:50:48 +0200

    Edited package documentation

--- src/main/scala/eu/cdevreeze/yaidom/package.scala
+++ src/main/scala/eu/cdevreeze/yaidom/package.scala
@@ -20,26 +20,26 @@ package eu.cdevreeze
  * <em>Yaidom</em> is yet another Scala immutable DOM-like XML API. The other well-known Scala immutable DOM-like APIs are
  * the standard scala.xml API and Anti-XML. The latter API is considered by many to be an improvement over the former, but both APIs:
  * <ul>
- * <li>attempt to offer XPath-like querying, thus somewhat blurring the distinction between nodes and node collections</li>
+ * <li>attempt to offer an XPath-like querying experience, thus somewhat blurring the distinction between nodes and node collections</li>
  * <li>lack first-class support for namespaces (and namespace URIs)</li>
  * <li>have limited (functional) update support</li>
  * </ul>
  *
  * Yaidom takes a different approach, avoiding XPath-like query support, and offering good namespace and decent (functional) update
- * support. Yaidom also values <em>mathematical precision</em> and clarity. Still, the API remains practical and
+ * support. Yaidom is also characterized by <em>mathematical precision</em> and clarity. Still, the API remains practical and
  * pragmatic. In particular, the API user has much configuration control over parsing and serialization, because yaidom exposes
  * the underlying JAXP parsers and serializers, which can be configured by the library user.
  *
- * Yaidom chose its battles. For example, knowing that DTDs do not know about namespaces, yaidom offers good namespace
+ * Yaidom chooses its battles. For example, given that DTDs do not know about namespaces, yaidom offers good namespace
  * support, but ignores DTDs entirely. Of course the underlying XML parser may still validate XML against a DTD, if so desired.
- * As another example, yaidom tries to leave the handling of gory details of XML processing (such as whitespace handling)
+ * As another example, yaidom tries to leave the handling of the gory details of XML processing (such as whitespace handling)
  * as much as possible to JAXP (and JAXP parser/serializer configuration). As yet another example, yaidom knows nothing about
  * (XML Schema) types of elements and attributes.
  *
  * Yaidom, and in particular this package, contains the following layers:
  * <ol>
  * <li><em>basic concepts</em>, such as (qualified and expanded) names of elements and attributes</li>
- * <li>the <em>uniform query API traits</em>, to query elements for descendant elements</li>
+ * <li>the <em>uniform query API traits</em>, to query elements for child, descendant and descendant-or-self elements</li>
  * <li>some of the specific <em>element implementations</em>, mixing in those uniform query API traits</li>
  * </ol>
  *
@@ -50,8 +50,9 @@ package eu.cdevreeze
  * In real world XML, elements (and sometimes attributes) tend to have names within a certain namespace. There are 2 kinds of names
  * at play here:
  * <ul>
- * <li><em>qualified names</em>, such as `book:Title` and `Edition`</li>
- * <li><em>expanded names</em>, such as `{http://bookstore/book}Title` (in James Clark notation) and `Edition`</li>
+ * <li><em>qualified names</em>: prefixed names, such as `book:Title`, and unprefixed names, such as `Edition`</li>
+ * <li><em>expanded names</em>: having a namespace, such as `{http://bookstore/book}Title` (in James Clark notation),
+ * and not having a namespace, such as `Edition`</li>
  * </ul>
  * They are represented by immutable classes [[eu.cdevreeze.yaidom.QName]] and [[eu.cdevreeze.yaidom.EName]], respectively.
  *
@@ -110,7 +111,7 @@ package eu.cdevreeze
  *
  * Consider the last element with qualified name `QName("book:Book")`. To resolve this qualified name as expanded name, we need to
  * know the namespaces in scope at that element. To compute the in-scope namespaces, we need to accumulate the namespace
- * declarations of the last `book:Book` element and of its ancestor elements, starting with the root element.
+ * declarations of the last `book:Book` element and of its ancestor element(s), starting with the root element.
  *
  * The start Scope is "parent scope" `Scope.Empty`. Then, in the root element we find namespace declarations:
  * {{{
@@ -124,7 +125,7 @@ package eu.cdevreeze
  * {{{
  * Scope.from("book" -> "http://bookstore/book", "auth" -> "http://bookstore/author")
  * }}}
- * We find no other namespace declarations in the ancestry of the last `book:Book` element, so the computed scope is also the scope
+ * We find no other namespace declarations in the last `book:Book` element or its ancestor(s), so the computed scope is also the scope
  * of the last `book:Book` element.
  *
  * Then `QName("book:Book")` is resolved as follows:
@@ -174,7 +175,7 @@ package eu.cdevreeze
  * Using the Scope mentioned earlier, the latter element path builder resolves to the element path given before that, by
  * invoking method `ElemPathBuilder.build(scope)`. In order for this to work, the Scope must be <em>invertible</em>. That is,
  * there must be a one-to-one correspondence between prefixes ("" for the default namespace) and namespace URIs, because
- * otherwise the index numbers may not match. Also note that the prefixes `book` and `auth` in the element path builder are
+ * otherwise the index numbers may differ. Also note that the prefixes `book` and `auth` in the element path builder are
  * arbitrary, and need not match with the prefixes used in the XML tree itself.
  *
  * ==Uniform query API traits==
@@ -195,11 +196,11 @@ package eu.cdevreeze
  * }}}
  * The result would be an immutable IndexedSeq of `ScalaXmlElem` instances, holding 2 book elements.
  *
- * We could have written:
+ * We could instead have written:
  * {{{
  * bookstoreElem.filterElemsOrSelf(EName("{http://bookstore/book}Book"))
  * }}}
- * instead, with the same result.
+ * with the same result.
  *
  * Instead of searching for appropriate descendant-or-self elements, we could have searched for <em>descendant</em> elements only,
  * without altering the result in this case:
@@ -227,21 +228,29 @@ package eu.cdevreeze
  * We could find all authors of the Scala book as follows:
  * {{{
  * for {
- *   bookElem <- bookstoreElem.filterChildElems(EName("{http://bookstore/book}Book"))
- *   if bookElem.attributeOption(EName("ISBN")) == Some("978-0981531649")
- *   authorElem <- bookElem.filterElems(EName("{http://bookstore/author}Author"))
- * } yield authorElem
- * }}}
- * which is equivalent to:
- * {{{
- * for {
  *   bookElem <- bookstoreElem filterChildElems (elem => elem.resolvedName == EName("{http://bookstore/book}Book"))
  *   if bookElem.attributeOption(EName("ISBN")) == Some("978-0981531649")
  *   authorElem <- bookElem filterElems (elem => elem.resolvedName == EName("{http://bookstore/author}Author"))
  * } yield authorElem
  * }}}
+ * or:
+ * {{{
+ * for {
+ *   bookElem <- bookstoreElem.filterChildElems(EName("{http://bookstore/book}Book"))
+ *   if bookElem.attributeOption(EName("ISBN")) == Some("978-0981531649")
+ *   authorElem <- bookElem.filterElems(EName("{http://bookstore/author}Author"))
+ * } yield authorElem
+ * }}}
  *
  * We could even use operator notation, as follows:
+ * {{{
+ * for {
+ *   bookElem <- bookstoreElem \ (elem => elem.resolvedName == EName("{http://bookstore/book}Book"))
+ *   if (bookElem \@ EName("ISBN")) == Some("978-0981531649")
+ *   authorElem <- bookElem \\ (elem => elem.resolvedName == EName("{http://bookstore/author}Author"))
+ * } yield authorElem
+ * }}}
+ * or:
  * {{{
  * for {
  *   bookElem <- bookstoreElem \ EName("{http://bookstore/book}Book")
@@ -252,25 +261,25 @@ package eu.cdevreeze
  * where `\\` stands for `filterElemsOrSelf`.
  *
  * Now suppose the same XML is stored in a (org.w3c.dom) DOM tree, wrapped in a [[eu.cdevreeze.yaidom.dom.DomElem]] `bookstoreElem`.
- * Then the same queries would use exactly the same code as above. The result would be a collection of `DomElem` instances
+ * Then the same queries would use exactly the same code as above! The result would be a collection of `DomElem` instances
  * instead of `ScalaXmlElem` instances, however. There are many more element implementations in yaidom, and they share
  * (most of) the same query API. Therefore this query API is called a <em>uniform</em> query API.
  *
- * The last example, using operator notation, looks a bit more "XPath-like". Yet it is more verbose than queries in Scala XML,
+ * The last example, using operator notation, looks a bit more "XPath-like". It is more verbose than queries in Scala XML, however,
  * partly because in yaidom these operators cannot be chained. Yet this is with good reason. Yaidom does not blur the
  * distinction between elements and element collections, and therefore does not offer any XPath experience. The small price
  * paid in verbosity is made up for by precision. The yaidom query API traits have very precise definitions of their
  * operations, as can be seen in the corresponding documentation.
  *
- * The uniform query API traits turn minimal APIs into richer APIs, where these richer APIs are defined very precisely in terms
+ * The uniform query API traits turn minimal APIs into richer APIs, where each richer API is defined very precisely in terms
  * of the minimal API. The top-level query API trait is [[eu.cdevreeze.yaidom.ParentElemLike]]. It needs to be given a method
- * to query for child elements (not child nodes in general, but just child elements!), and it offers methods to query
+ * implementation to query for child elements (not child nodes in general, but just child elements!), and it offers methods to query
  * for some or all child elements, descendant elements, and descendant-or-self elements. That is, the minimal API consists
  * of abstract method `findAllChildElems`, and it offers methods such as `filterChildElems`, `filterElems` and `filterElemsOrSelf`.
- * This trait has no knowledge about elements at all, other than the fact that elements can have child elements.
+ * This trait has no knowledge about elements at all, other than the fact that <em>elements can have child elements</em>.
  *
  * Sub-trait [[eu.cdevreeze.yaidom.ElemLike]] adds minimal knowledge about elements themselves, viz. that elements have a
- * "resolved" (or expanded) name, and "resolved" attributes (mapping attribute expanded names to attribute values). That is,
+ * <em>"resolved"</em> (or expanded) name, and "resolved" attributes (mapping attribute expanded names to attribute values). That is,
  * it needs to be given implementations of abstract methods `resolvedName` and `resolvedAttributes`, and then offers methods to
  * query for attributes or child/descendant/descendant-or-self elements with a given expanded name. The trait is trivially defined
  * in terms of its super-trait.
@@ -285,21 +294,20 @@ package eu.cdevreeze
  * element knowledge that traits `ElemLike` and `ParentElemLike` have about elements. Therefore the query code can be used
  * unchanged for different element implementations.
  *
- * The `ElemLike` trait has sub-trait [[eu.cdevreeze.yaidom.PathAwareElemLike]]. It adds knowledge about element paths. Element
+ * The `ElemLike` trait has sub-trait [[eu.cdevreeze.yaidom.PathAwareElemLike]]. It adds knowledge about <em>element paths</em>. Element
  * paths can be queried (in the same way that elements can be queried in trait `ParentElemLike`), and elements can be found
  * given an element path.
  *
- * For example, to query for the element paths of the above-mentioned Scala book authors (relative to the root element), the following
- * code can be used (if the used element implementation mixes in trait `PathAwareElemLike`, which is not the case for the Scala XML and
- * DOM wrappers above):
+ * For example, to query for the Scala book authors, the following alternative code can be used (if the used element
+ * implementation mixes in trait `PathAwareElemLike`, which is not the case for the Scala XML and DOM wrappers above):
  * {{{
  * for {
  *   authorElemPath <- bookstoreElem filterElemOrSelfPaths (elem => elem.resolvedName == EName("{http://bookstore/author}Author"))
  *   if authorElemPath.entries.contains(ElemPath.Entry(EName("{http://bookstore/book}Book"), 1))
- * } yield authorElemPath
+ * } yield bookstoreElem.getWithElemPath(authorElemPath)
  * }}}
  *
- * The `PathAwareElemLike` trait has sub-trait [[eu.cdevreeze.yaidom.UpdatableElemLike]]. This trait offers functional updates
+ * The `PathAwareElemLike` trait has sub-trait [[eu.cdevreeze.yaidom.UpdatableElemLike]]. This trait offers <em>functional updates</em>
  * at given element paths. Whereas the super-traits know only about elements, this trait knows that elements have some node
  * super-type.
  *
@@ -313,7 +321,7 @@ package eu.cdevreeze
  * implementations. In this package there are 2 immutable element implementations, [[eu.cdevreeze.yaidom.ElemBuilder]]
  * and [[eu.cdevreeze.yaidom.Elem]].
  *
- * Class [[eu.cdevreeze.yaidom.Elem]] is the default element implementation of yaidom. It extends class [[eu.cdevreeze.yaidom.Node]].
+ * Class [[eu.cdevreeze.yaidom.Elem]] is the <em>default element implementatio</em>n of yaidom. It extends class [[eu.cdevreeze.yaidom.Node]].
  * The latter also has sub-classes for text nodes, comments, entity references and processing instructions. Class [[eu.cdevreeze.yaidom.Document]]
  * contains a document `Elem`, but is not a `Node` sub-class itself.
  *
@@ -326,7 +334,7 @@ package eu.cdevreeze
  * <li>Besides the tag name, attributes and child nodes, it keeps a `Scope`, but no `Declarations`</li>
  * <li>This makes it easy to compose these elements, as long as scopes are passed explicitly throughout the element tree</li>
  * <li>Equality is reference equality, because it is hard to come up with a sensible equality for this element class</li>
- * <li>Roundtripping cannot be entirely lossless, but this class does try to keep the attribute order (although irrelevant according to XML InfoSet)</li>
+ * <li>Roundtripping cannot be entirely lossless, but this class does try to retain the attribute order (although irrelevant according to XML InfoSet)</li>
  * <li>Sub-packages `parse` and `print` offer `DocumentParser` and `DocumentPrinter` classes for parsing/serializing these default `Elem` (and `Document`) instances</li>
  * </ul>
  *
@@ -334,14 +342,14 @@ package eu.cdevreeze
  * The latter is not needed if we use class [[eu.cdevreeze.yaidom.ElemBuilder]] to create element trees by hand. When the tree
  * has been fully created as `ElemBuilder`, invoke method `ElemBuilder.build(parentScope)` to turn it into an `Elem`.
  *
- * Classes `Elem` and `ElemBuilder`, like their super-classes `Node` and `NodeBuilder`, have very much in common. Both are immutable,
+ * Like their super-classes `Node` and `NodeBuilder`, classes `Elem` and `ElemBuilder` have very much in common. Both are immutable,
  * easy to compose (`ElemBuilder` instances even more so), equality is reference equality, etc. The most important differences
  * are as follows:
  * <ul>
  * <li>Instead of a `Scope`, an `ElemBuilder` contains a `Declarations`</li>
  * <li>This makes an `ElemBuilder` easier to compose than an `Elem`, because no Scope needs to be passed around throughout the tree</li>
  * <li>Class `ElemBuilder` uses a minimal query API, mixing in only traits `ParentElemLike` and `TransformableElemLike`</li>
- * <li>After all, an `ElemBuilder` neither keeps nor knows about Scopes</li>
+ * <li>After all, an `ElemBuilder` neither keeps nor knows about Scopes, so does not know about resolved element/attribute names</li>
  * </ul>
  *
  * The Effective Java book element in the XML example above could have been written as `ElemBuilder` (without the inter-element whitespace) as follows:
@@ -426,13 +434,12 @@ package eu.cdevreeze
  * Yaidom has the following packages, and layering between packages:
  * <ol>
  * <li>Package [[eu.cdevreeze.yaidom]], with the content described above. It depends on no other yaidom packages.</li>
- * <li>Package [[eu.cdevreeze.yaidom.convert]]. It contains conversions between yaidom and DOM, Scala XML, etc.
- * This package depends on the yaidom root package.</li>
+ * <li>Package [[eu.cdevreeze.yaidom.convert]]. It contains conversions between default yaidom nodes on the one hand and DOM,
+ * Scala XML, etc. on the other hand. The `convert` package depends on the yaidom root package.</li>
  * <li>Packages [[eu.cdevreeze.yaidom.parse]] and [[eu.cdevreeze.yaidom.print]], for parsing/printing Elems. They depend on
  * the packages mentioned above.</li>
- * <li>The other packages: [[eu.cdevreeze.yaidom.dom]], [[eu.cdevreeze.yaidom.indexed]], [[eu.cdevreeze.yaidom.resolved]],
- * [[eu.cdevreeze.yaidom.scalaxml]] and [[eu.cdevreeze.yaidom.xlink]]. They depend on (some of) the packages mentioned above,
- * and not on each other.</li>
+ * <li>The other packages: [[eu.cdevreeze.yaidom.dom]], [[eu.cdevreeze.yaidom.indexed]], [[eu.cdevreeze.yaidom.resolved]] and
+ * [[eu.cdevreeze.yaidom.scalaxml]]. They depend on (some of) the packages mentioned above, and not on each other.</li>
  * </ol>
  * Indeed, all yaidom package dependencies are uni-directional.
  *
