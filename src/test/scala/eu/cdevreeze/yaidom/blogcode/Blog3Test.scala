2412aff549a9cf42e67a6b4650d3454b8e7aee76
Chris de Vreeze <chris.de.vreeze@caiway.net>
2014-09-26 00:12:59 +0200

    Started on code for next blog post

--- src/test/scala/eu/cdevreeze/yaidom/blogcode/Blog3Test.scala
+++ src/test/scala/eu/cdevreeze/yaidom/blogcode/Blog3Test.scala
@@ -23,10 +23,17 @@ import org.scalatest.{ Suite, BeforeAndAfterAll }
 import org.scalatest.junit.JUnitRunner
 
 /**
- * Code of yaidom blog 3 ("advanced yaidom querying"). The blog uses examples from the coursera course Introduction to Databases,
- * by Jennifer Widom.
+ * Code of yaidom blog 3 ("more yaidom querying"). The blog uses examples from the XML Schema Part 0 (Primer).
  *
  * Encourage the reader to play with Scala and yaidom in the REPL.
+ *
+ * The example "problem" is to find local element declarations in a schema, and match them with elements in the instance
+ * document. To achieve that, we must be able to reason about the found element declarations. For example, we want to
+ * determine the target namespace, and the surrounding content model.
+ *
+ * We will gradually introduce the different query API traits, and mention some element classes implementing them.
+ * Each query API sub-trait has something to offer (w.r.t. the problem mentioned above) that its super-trait does
+ * not offer, in order to make matching between element declaration and elements in the instance document easier.
  *
  * @author Chris de Vreeze
  */
@@ -34,18 +41,15 @@ import org.scalatest.junit.JUnitRunner
 class Blog3Test extends Suite {
 
   private val pathToParentDir: java.io.File =
-    (new java.io.File(classOf[Blog3Test].getResource("books.xml").toURI)).getParentFile
+    (new java.io.File(classOf[Blog3Test].getResource("po1.xsd").toURI)).getParentFile
 
-  /**
-   * The code in this test can be copied to the "introductory example" section in the third article on yaidom.
-   */
-  @Test def testIntroductoryExample(): Unit = {
+  @Test def testParentElemLikeExample(): Unit = {
     import java.io.File
     import javax.xml.parsers._
     import scala.collection.immutable
     import eu.cdevreeze.yaidom._
 
-    val ns = "http://bookstore"
+    val tns = "http://www.example.com/PO1"
 
     // Using a yaidom DocumentParser that used SAX internally
     val docParser = parse.DocumentParserUsingSax.newInstance
@@ -53,47 +57,138 @@ class Blog3Test extends Suite {
     // Replace the following path!
     val parentDir = new File(pathToParentDir.getPath)
 
-    val doc: Document =
-      docParser.parse(new File(parentDir, "books.xml"))
+    val xsdDoc: Document =
+      docParser.parse(new File(parentDir, "po1.xsd"))
 
-    val docElem = doc.documentElement
+    val xmlDoc: Document =
+      docParser.parse(new File(parentDir, "po1.xml"))
 
-    // The example, showing (bottom-up) queries using attributes, paths, and XML literals
+    // Turn XSD into an ElemBuilder, which cannot be queried about scopes and expanded names
+    // Normally one would turn ElemBuilders (once "ready") into Elems, and not the other way around.
 
-    import ElemApi._
+    val xsdElemBuilder = NodeBuilder.fromElem(xsdDoc.documentElement)(Scope.Empty)
+    val xmlElemBuilder = NodeBuilder.fromElem(xmlDoc.documentElement)(Scope.Empty)
 
-    def findIsbnsOfBooksBy(firstName: String, lastName: String): immutable.IndexedSeq[String] = {
-      for {
-        authorPath <- docElem filterElemPaths withEName(ns, "Author")
-        authorElem = docElem.getElemOrSelfByPath(authorPath)
-        if authorElem.getChildElem(EName(ns, "First_Name")).text == firstName &&
-          authorElem.getChildElem(EName(ns, "Last_Name")).text == lastName
-        bookPath <- authorPath findAncestorPath (path => path.endsWithName(EName(ns, "Book")))
-        bookElem = docElem.getElemOrSelfByPath(bookPath)
-        isbn <- bookElem \@ EName("ISBN")
-      } yield isbn
-    }
+    // Needing more than the ParentElemLike API
+    // Note that we do not even know if the element declarations found are local instead of global
+    val shipToElemDecls =
+      xsdElemBuilder filterElems { e =>
+        e.qname.localPart == "element" &&
+          e.attributes.toMap.get(QName("name")) == Some("shipTo")
+      }
+    require(shipToElemDecls.size == 1, s"Expected precisely 1 element declaration for 'shipTo'")
+
+    val shipToElems =
+      xmlElemBuilder filterElems { e =>
+        e.qname.localPart == "billTo"
+      }
+    require(shipToElems.size == 1, s"Expected precisely 1 element named 'shipTo'")
+
+    // But how do we know this element declaration and element match? We do not know (target) namespace, and we do
+    // not know the (expected) element ancestry. Clearly, trait ParentElemLike does not really help us here.
+    // That trait only knows that elements can have child elements, and as a consequence descendant and descendant-or-self
+    // elements, but it knows nothing more about elements, not even the name and attributes.
+  }
+
+  @Test def testElemLikeExample(): Unit = {
+    import java.io.File
+    import javax.xml.parsers._
+    import scala.collection.immutable
+    import eu.cdevreeze.yaidom._
+    import eu.cdevreeze.yaidom.ElemApi._
+
+    val xsNs = "http://www.w3.org/2001/XMLSchema"
+    val tns = "http://www.example.com/PO1"
+
+    // Using a yaidom DocumentParser that used SAX internally
+    val docParser = parse.DocumentParserUsingSax.newInstance
+
+    // Replace the following path!
+    val parentDir = new File(pathToParentDir.getPath)
+
+    val xsdDoc: Document =
+      docParser.parse(new File(parentDir, "po1.xsd"))
+
+    val xmlDoc: Document =
+      docParser.parse(new File(parentDir, "po1.xml"))
+
+    // Note that we do not even know if the element declarations found are local instead of global
+    val shipToElemDecls =
+      xsdDoc.documentElement filterElems { e =>
+        e.resolvedName == EName(xsNs, "element") &&
+          e.attributeOption(EName("name")) == Some("shipTo")
+      }
+    require(shipToElemDecls.size == 1, s"Expected precisely 1 element declaration for 'shipTo'")
+
+    val shipToElems =
+      xmlDoc.documentElement filterElems { e =>
+        e.localName == "billTo"
+      }
+    require(shipToElems.size == 1, s"Expected precisely 1 element named 'shipTo'")
+
+    // But how do we know this element declaration and element match? We do not know (target) namespace, and we do
+    // not know the (expected) element ancestry. Also trait ElemLike does not really help us enough here.
+    // That trait only knows about elements what trait ParentElemLike knows, plus the fact the elements have
+    // expanded names and attributes with expanded names.
+
+    // Now use Scala XML wrappers, and show equivalent results.
+
+    val xsdElemAsScalaXmlWrapper: scalaxml.ScalaXmlElem =
+      scalaxml.ScalaXmlElem(scala.xml.XML.loadFile(new File(parentDir, "po1.xsd")))
+
+    val xmlElemAsScalaXmlWrapper: scalaxml.ScalaXmlElem =
+      scalaxml.ScalaXmlElem(scala.xml.XML.loadFile(new File(parentDir, "po1.xml")))
+
+    // Using the same ElemLike query API in exactly the same way
+
+    val shipToElemDeclsAsScalaXmlWrappers =
+      xsdElemAsScalaXmlWrapper filterElems { e =>
+        e.resolvedName == EName(xsNs, "element") &&
+          e.attributeOption(EName("name")) == Some("shipTo")
+      }
+    require(shipToElemDeclsAsScalaXmlWrappers.size == 1, s"Expected precisely 1 element declaration for 'shipTo'")
+
+    val shipToElemsAsScalaXmlWrappers =
+      xmlElemAsScalaXmlWrapper filterElems { e =>
+        e.localName == "billTo"
+      }
+    require(shipToElemsAsScalaXmlWrappers.size == 1, s"Expected precisely 1 element named 'shipTo'")
+
+    // Now show equivalence
 
     require(
-      findIsbnsOfBooksBy("Jeffrey", "Ullman") ==
-        List("ISBN-0-13-713526-2", "ISBN-0-13-815504-6", "ISBN-0-11-222222-3"))
-
-    def summarizeBook(isbn: String): Elem = {
-      val bookElem =
-        (docElem findElem { e => e.resolvedName == EName(ns, "Book") && (e \@ EName("ISBN")) == Some(isbn) }).get
-
-      val xml =
-        <Book xmlns="http://bookstore" isbn={ bookElem.attribute(EName("ISBN")) } title={ bookElem.getChildElem(EName(ns, "Title")).text }>
-          <Authors>{
-            val authorElems = bookElem.filterElems(withEName(ns, "Author"))
-            authorElems.map(e => s"${e.getChildElem(withLocalName("First_Name")).text} ${e.getChildElem(withLocalName("Last_Name")).text}").mkString(", ")
-          }</Authors>
-        </Book>
-      convert.ScalaXmlConversions.convertToElem(xml)
-    }
+      resolved.Elem(shipToElemDecls.head).removeAllInterElementWhitespace ==
+        resolved.Elem(convert.ScalaXmlConversions.convertToElem(shipToElemDeclsAsScalaXmlWrappers.head.wrappedNode)).removeAllInterElementWhitespace)
 
     require(
-      summarizeBook("ISBN-0-13-713526-2").getChildElem(withLocalName("Authors")).text ==
-        "Jeffrey Ullman, Jennifer Widom")
+      resolved.Elem(shipToElems.head).removeAllInterElementWhitespace ==
+        resolved.Elem(convert.ScalaXmlConversions.convertToElem(shipToElemsAsScalaXmlWrappers.head.wrappedNode)).removeAllInterElementWhitespace)
+
+    // We could have queried the resolved elements themselves, using the same query API in the same way
+
+    val shipToElemDeclsAsResolvedElems =
+      resolved.Elem(xsdDoc.documentElement) filterElems { e =>
+        e.resolvedName == EName(xsNs, "element") &&
+          e.attributeOption(EName("name")) == Some("shipTo")
+      }
+    require(shipToElemDeclsAsResolvedElems.size == 1, s"Expected precisely 1 element declaration for 'shipTo'")
+
+    val shipToElemsAsResolvedElems =
+      resolved.Elem(xmlDoc.documentElement) filterElems { e =>
+        e.localName == "billTo"
+      }
+    require(shipToElemsAsResolvedElems.size == 1, s"Expected precisely 1 element named 'shipTo'")
+
+    require(
+      resolved.Elem(shipToElemDecls.head).removeAllInterElementWhitespace ==
+        shipToElemDeclsAsResolvedElems.head.removeAllInterElementWhitespace)
+
+    require(
+      resolved.Elem(shipToElems.head).removeAllInterElementWhitespace ==
+        shipToElemsAsResolvedElems.head.removeAllInterElementWhitespace)
+  }
+
+  @Test def testNavigableElemLikeExample(): Unit = {
+    // TODO Use indexed or docaware elements; context matters! We use more than the NavigableElemLike trait, though!
   }
 }
