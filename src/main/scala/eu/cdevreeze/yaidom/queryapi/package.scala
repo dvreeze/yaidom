6597b48fd157448be80366b375f7513b07756930
Chris de Vreeze <chris.de.vreeze@caiway.net>
2014-10-13 21:56:23 +0200

    Added comment about migration from 1.0

--- src/main/scala/eu/cdevreeze/yaidom/queryapi/package.scala
+++ src/main/scala/eu/cdevreeze/yaidom/queryapi/package.scala
@@ -22,6 +22,31 @@ package eu.cdevreeze.yaidom
  *
  * This package depends only on the core package in yaidom, but many other packages do depend on this one.
  *
+ * ==Notes on migration from yaidom 1.0 to 1.1 and later==
+ *
+ * When migrating from yaidom 1.0 to 1.1 or later, there are many breaking changes in the query API traits to deal
+ * with. Not only was the query API moved to its own package, but also was the inheritance hierarchy (of 1.0)
+ * abandoned, and were some traits renamed. The most important renaming was from `ParentElemApi` to `ElemApi`,
+ * where the 1.0 `ElemApi` became `ElemApi with HasENameApi`.
+ *
+ * The query API traits in 1.1 and later are more orthogonal (after all, the inheritance hierarchy is no longer there).
+ * We can even simulate the 1.0 query API traits by combining 1.1 traits, keeping renaming in mind:
+ * <ul>
+ * <li>ParentElemApi (1.0) --> ElemApi (1.1)</li>
+ * <li>ElemApi (1.0) --> ElemApi with HasENameApi (1.1)</li>
+ * <li>NavigableElemApi (1.0) --> ElemApi with HasENameApi with IsNavigableApi (1.1)</li>
+ * <li>PathAwareElemApi (1.0) --> ElemApi with HasENameApi with IsNavigableApi with PathAwareElemApi (1.1)</li>
+ * <li>UpdatableElemApi (1.0) --> ElemApi with HasENameApi with PathAwareElemApi with UpdatableElemApi (1.1)</li>
+ * <li>TransformableElemApi (1.0) --> TransformableElemApi (1.1)</li>
+ * <li>SubtypeAwareParentElemApi (1.0) --> SubtypeAwareElemApi (1.1)</li>
+ * </ul>
+ *
+ * The fact that the 1.0 query API traits can be simulated by (combinations of) 1.1 query API traits shows how
+ * orthogonality of the query API has improved. For example, now we can abstract over `indexed` and `docaware`
+ * elements by using the following type: `ElemApi[E] with HasPathApi with IsNavigableApi[E]`. This is a very
+ * powerful type, supporting queries using predicates in which the elements themselves contain Paths (that can be
+ * navigated to).
+ *
  * @author Chris de Vreeze
  */
 package object queryapi
