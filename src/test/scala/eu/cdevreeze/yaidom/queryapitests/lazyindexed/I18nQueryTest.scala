314eedfc322afa3bfc482b64cdb3a966579650e7
Chris de Vreeze <chris.de.vreeze@caiway.net>
2016-05-11 23:50:48 +0200

    More lazy indexed element tests

---
+++ src/test/scala/eu/cdevreeze/yaidom/queryapitests/lazyindexed/I18nQueryTest.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2011-2014 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.yaidom.queryapitests.lazyindexed
+
+import java.{ util => jutil }
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import eu.cdevreeze.yaidom.convert
+import eu.cdevreeze.yaidom.core.Scope
+import eu.cdevreeze.yaidom.indexed.LazyIndexedScopedElem
+import eu.cdevreeze.yaidom.queryapitests.AbstractI18nQueryTest
+import eu.cdevreeze.yaidom.resolved
+import eu.cdevreeze.yaidom.simple
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
+
+/**
+ * Query test case for lazy indexed elements.
+ *
+ * @author Chris de Vreeze
+ */
+@RunWith(classOf[JUnitRunner])
+class I18nQueryTest extends AbstractI18nQueryTest {
+
+  private val logger: jutil.logging.Logger = jutil.logging.Logger.getLogger("eu.cdevreeze.yaidom.queryapitests.lazyindexed")
+
+  final type E = LazyIndexedScopedElem[simple.Elem]
+
+  protected final val rootElem: LazyIndexedScopedElem[simple.Elem] = {
+    val dbf = DocumentBuilderFactory.newInstance
+
+    def createDocumentBuilder(documentBuilderFactory: DocumentBuilderFactory): DocumentBuilder = {
+      val db = documentBuilderFactory.newDocumentBuilder()
+      db
+    }
+
+    val is = classOf[I18nQueryTest].getResourceAsStream("/eu/cdevreeze/yaidom/queryapitests/internationalization-instance-valid.xml")
+
+    val domDoc = createDocumentBuilder(dbf).parse(is)
+
+    LazyIndexedScopedElem[simple.Elem](
+      Some(new java.net.URI("")),
+      convert.DomConversions.convertToElem(domDoc.getDocumentElement(), Scope.Empty))
+  }
+
+  protected final def toResolvedElem(elem: E): resolved.Elem =
+    resolved.Elem(elem.elem)
+}
