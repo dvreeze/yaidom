64078feb72e87ee76902b1c3ea030eec71eda46c
Chris de Vreeze <chris.de.vreeze@caiway.net>
2016-05-11 22:43:39 +0200

    Added lazy indexed documents

---
+++ src/main/scala/eu/cdevreeze/yaidom/indexed/LazyDocument.scala
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2011-2014 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.yaidom.indexed
+
+import java.net.URI
+
+import scala.Vector
+import scala.collection.immutable
+
+import eu.cdevreeze.yaidom.core.XmlDeclaration
+import eu.cdevreeze.yaidom.queryapi.Nodes
+import eu.cdevreeze.yaidom.queryapi.XmlBaseSupport
+import eu.cdevreeze.yaidom.simple
+import eu.cdevreeze.yaidom.simple.Comment
+import eu.cdevreeze.yaidom.simple.ProcessingInstruction
+
+/**
+ * `LazyIndexedDocument`, containing a lazy "indexed" document element with simple elements as underlying elements.
+ *
+ * @author Chris de Vreeze
+ */
+final class LazyDocument(
+  xmlDeclarationOption: Option[XmlDeclaration],
+  children: immutable.IndexedSeq[Nodes.CanBeDocumentChild]) extends LazyIndexedDocument[simple.Elem](xmlDeclarationOption, children) with Immutable {
+
+  def document: simple.Document =
+    simple.Document(
+      uriOption,
+      xmlDeclarationOption,
+      children map {
+        case e: Nodes.Elem                   => documentElement.elem
+        case pi: Nodes.ProcessingInstruction => ProcessingInstruction(pi.target, pi.data)
+        case c: Nodes.Comment                => Comment(c.text)
+      })
+
+  /** Creates a copy, but with the new documentElement passed as parameter newRoot */
+  def withDocumentElement(newRoot: Elem): LazyDocument = new LazyDocument(
+    xmlDeclarationOption = this.xmlDeclarationOption,
+    children = this.children map {
+      case elm: Nodes.Elem                => newRoot
+      case node: Nodes.CanBeDocumentChild => node
+    })
+
+  /** Creates a copy, but with the new xmlDeclarationOption passed as parameter newXmlDeclarationOption */
+  def withXmlDeclarationOption(newXmlDeclarationOption: Option[XmlDeclaration]): LazyDocument = new LazyDocument(
+    xmlDeclarationOption = newXmlDeclarationOption,
+    children = this.children)
+}
+
+object LazyDocument {
+
+  def apply(
+    xmlDeclarationOption: Option[XmlDeclaration],
+    children: immutable.IndexedSeq[Nodes.CanBeDocumentChild]): LazyDocument = {
+
+    new LazyDocument(xmlDeclarationOption, children)
+  }
+
+  def apply(
+    xmlDeclarationOption: Option[XmlDeclaration],
+    documentElement: LazyIndexedScopedElem[simple.Elem],
+    processingInstructions: immutable.IndexedSeq[ProcessingInstruction] = immutable.IndexedSeq(),
+    comments: immutable.IndexedSeq[Comment] = immutable.IndexedSeq()): LazyDocument = {
+
+    new LazyDocument(
+      xmlDeclarationOption,
+      processingInstructions ++ comments ++ Vector(documentElement))
+  }
+
+  def apply(documentElement: LazyIndexedScopedElem[simple.Elem]): LazyDocument = {
+    new LazyDocument(None, Vector(documentElement))
+  }
+
+  /**
+   * Returns `apply(d.withUriOption(Some(docUri)))`.
+   */
+  def apply(docUri: URI, d: simple.Document): LazyDocument = {
+    apply(d.withUriOption(Some(docUri)))
+  }
+
+  /**
+   * Creates a "lazy document" from a simple document.
+   */
+  def apply(d: simple.Document): LazyDocument = {
+    val elem = LazyIndexedScopedElem(d.uriOption, d.documentElement)
+
+    val children = d.children map {
+      case elm: Nodes.Elem                => elem
+      case node: Nodes.CanBeDocumentChild => node
+    }
+
+    apply(d.xmlDeclarationOption, children)
+  }
+}
---
+++ src/main/scala/eu/cdevreeze/yaidom/indexed/LazyIndexedDocument.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2011-2014 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.yaidom.indexed
+
+import java.net.URI
+
+import scala.collection.immutable
+
+import eu.cdevreeze.yaidom.core.Path
+import eu.cdevreeze.yaidom.core.XmlDeclaration
+import eu.cdevreeze.yaidom.queryapi.DocumentApi
+import eu.cdevreeze.yaidom.queryapi.Nodes
+import eu.cdevreeze.yaidom.queryapi.ScopedElemApi
+
+/**
+ * Document, containing a lazy "indexed" document element.
+ *
+ * Note that class `LazyIndexedDocument` does not have any query methods for `Elem` instances. In particular, the `ElemApi` does not
+ * apply to documents. Therefore, given a document, querying for elements (other than the document element itself) always goes
+ * via the document element.
+ *
+ * @author Chris de Vreeze
+ */
+abstract class LazyIndexedDocument[U <: ScopedElemApi[U]](
+  val xmlDeclarationOption: Option[XmlDeclaration],
+  val children: immutable.IndexedSeq[Nodes.CanBeDocumentChild]) extends DocumentApi[LazyIndexedScopedElem[U]] with Immutable {
+
+  require(xmlDeclarationOption ne null)
+  require(children ne null)
+
+  require(
+    children.collect({ case elm: Nodes.Elem => elm }).size == 1,
+    s"A document must have exactly one child element")
+
+  require(
+    children.collect({ case elm: LazyIndexedScopedElem[U] => elm }).size == 1,
+    s"A document must have exactly one (LazyIndexedScopedElem) child element (${uriOption.map(_.toString).getOrElse("No URI found")})")
+
+  require(documentElement.path == Path.Empty, "The document element must have the root Path")
+
+  final def documentElement: LazyIndexedScopedElem[U] =
+    children.collect({ case elm: LazyIndexedScopedElem[U] => elm }).head
+
+  final def processingInstructions: immutable.IndexedSeq[Nodes.ProcessingInstruction] =
+    children.collect({ case pi: Nodes.ProcessingInstruction => pi })
+
+  final def comments: immutable.IndexedSeq[Nodes.Comment] =
+    children.collect({ case c: Nodes.Comment => c })
+
+  final def uriOption: Option[URI] = documentElement.docUriOption
+
+  /**
+   * Returns the document URI, falling back to the empty URI if absent.
+   */
+  final def uri: URI = uriOption.getOrElse(new URI(""))
+
+  def document: DocumentApi[U]
+
+  final override def toString: String = document.toString
+
+  /** Creates a copy, but with the new documentElement passed as parameter newRoot */
+  def withDocumentElement(newRoot: LazyIndexedScopedElem[U]): LazyIndexedDocument[U]
+
+  /** Creates a copy, but with the new xmlDeclarationOption passed as parameter newXmlDeclarationOption */
+  def withXmlDeclarationOption(newXmlDeclarationOption: Option[XmlDeclaration]): LazyIndexedDocument[U]
+}
