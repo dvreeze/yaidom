950f0aa2e991b2103e9aeea63ec6ac6fca62f6de
Chris de Vreeze <chris.de.vreeze@caiway.net>
2016-09-23 20:15:21 +0200

    There is no need (anymore) to repeat super-class methods in BackingElemApi

--- src/main/scala/eu/cdevreeze/yaidom/queryapi/BackingElemApi.scala
+++ src/main/scala/eu/cdevreeze/yaidom/queryapi/BackingElemApi.scala
@@ -16,10 +16,6 @@
 
 package eu.cdevreeze.yaidom.queryapi
 
-import scala.collection.immutable
-
-import eu.cdevreeze.yaidom.core.Path
-
 /**
  * Shorthand for `IndexedScopedElemApi with HasParentApi`. In other words, this is an ancestry-aware "scoped element"
  * query API.
@@ -36,80 +32,6 @@ import eu.cdevreeze.yaidom.core.Path
 trait BackingElemApi extends IndexedScopedElemApi with HasParentApi {
 
   type ThisElem <: BackingElemApi
-
-  // Restricting AnyElemApi methods to use "this ThisElem", to prevent down-casts in code using this "raw" query API trait
-
-  override def thisElem: ThisElem
-
-  // Restricting ElemApi methods to use "this ThisElem", to prevent down-casts in code using this "raw" query API trait
-
-  override def findAllChildElems: immutable.IndexedSeq[ThisElem]
-
-  override def findAllElems: immutable.IndexedSeq[ThisElem]
-
-  override def findAllElemsOrSelf: immutable.IndexedSeq[ThisElem]
-
-  override def filterChildElems(p: ThisElem => Boolean): immutable.IndexedSeq[ThisElem]
-
-  override def filterElems(p: ThisElem => Boolean): immutable.IndexedSeq[ThisElem]
-
-  override def filterElemsOrSelf(p: ThisElem => Boolean): immutable.IndexedSeq[ThisElem]
-
-  override def findChildElem(p: ThisElem => Boolean): Option[ThisElem]
-
-  override def findElem(p: ThisElem => Boolean): Option[ThisElem]
-
-  override def findElemOrSelf(p: ThisElem => Boolean): Option[ThisElem]
-
-  override def findTopmostElems(p: ThisElem => Boolean): immutable.IndexedSeq[ThisElem]
-
-  override def findTopmostElemsOrSelf(p: ThisElem => Boolean): immutable.IndexedSeq[ThisElem]
-
-  override def getChildElem(p: ThisElem => Boolean): ThisElem
-
-  override def \(p: ThisElem => Boolean): immutable.IndexedSeq[ThisElem]
-
-  override def \\(p: ThisElem => Boolean): immutable.IndexedSeq[ThisElem]
-
-  override def \\!(p: ThisElem => Boolean): immutable.IndexedSeq[ThisElem]
-
-  // Restricting IsNavigableApi methods to use "this ThisElem", to prevent down-casts in code using this "raw" query API trait
-
-  override def findAllChildElemsWithPathEntries: immutable.IndexedSeq[(ThisElem, Path.Entry)]
-
-  override def findChildElemByPathEntry(entry: Path.Entry): Option[ThisElem]
-
-  override def getChildElemByPathEntry(entry: Path.Entry): ThisElem
-
-  override def findElemOrSelfByPath(path: Path): Option[ThisElem]
-
-  override def getElemOrSelfByPath(path: Path): ThisElem
-
-  override def findReverseAncestryOrSelfByPath(path: Path): Option[immutable.IndexedSeq[ThisElem]]
-
-  override def getReverseAncestryOrSelfByPath(path: Path): immutable.IndexedSeq[ThisElem]
-
-  // Restricting IndexedClarkElemApi methods to use "this ThisElem", to prevent down-casts in code using this "raw" query API trait
-
-  override def rootElem: ThisElem
-
-  override def reverseAncestryOrSelf: immutable.IndexedSeq[ThisElem]
-
-  override def reverseAncestry: immutable.IndexedSeq[ThisElem]
-
-  // Restricting HasParentApi methods to use "this ThisElem", to prevent down-casts in code using this "raw" query API trait
-
-  override def parentOption: Option[ThisElem]
-
-  override def parent: ThisElem
-
-  override def ancestorsOrSelf: immutable.IndexedSeq[ThisElem]
-
-  override def ancestors: immutable.IndexedSeq[ThisElem]
-
-  override def findAncestorOrSelf(p: ThisElem => Boolean): Option[ThisElem]
-
-  override def findAncestor(p: ThisElem => Boolean): Option[ThisElem]
 }
 
 object BackingElemApi {
