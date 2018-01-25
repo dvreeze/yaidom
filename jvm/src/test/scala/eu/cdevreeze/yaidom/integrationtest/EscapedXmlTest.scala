aa52f22d5ccdc16072459c12e18e7a37b9d58088
Chris de Vreeze <chris.de.vreeze@caiway.net>
2018-01-25 19:46:35 +0100

    Added test to EscapedXmlTest using a namespace

--- jvm/src/test/scala/eu/cdevreeze/yaidom/integrationtest/EscapedXmlTest.scala
+++ jvm/src/test/scala/eu/cdevreeze/yaidom/integrationtest/EscapedXmlTest.scala
@@ -103,4 +103,78 @@ class EscapedXmlTest extends FunSuite {
       resolved.Elem(innerXmlElem2).removeAllInterElementWhitespace
     }
   }
+
+  test("testRoundtripEscapedInnerXmlWithNamespace") {
+    val docPrinter = DocumentPrinterUsingDom.newInstance()
+
+    val docParser = DocumentParserUsingDom.newInstance()
+
+    val innerNs = "http://www.example.com/"
+
+    import simple.Node._
+
+    val scope = Scope.from("" -> innerNs)
+
+    val text1 = "This text contains no so-called predefined entities"
+    val text2 = "This text does contain some of the 5 predefined entities: <, >, &"
+
+    // Build the inner XML DOM tree, which will introduce the predefined entities for text2, once
+    // we serialize the DOM tree
+
+    val innerXmlElem =
+      emptyElem(QName("ul"), scope).
+        plusChild(textElem(QName("li"), scope, text1)).
+        plusChild(textElem(QName("li"), scope, text2))
+
+    val innerXmlString = docPrinter.print(innerXmlElem)
+
+    assertResult(true) {
+      innerXmlString.contains("""entities: &lt;, &gt;, &amp;""")
+    }
+    assertResult(true) {
+      innerXmlString.contains(s"""xmlns="$innerNs"""")
+    }
+
+    val outerXmlElem =
+      emptyElem(QName("report"), scope).
+        plusChild(textElem(QName("item"), scope, innerXmlString.trim))
+
+    val outerXmlString = docPrinter.print(outerXmlElem)
+
+    assertResult(true) {
+      outerXmlString.contains("""&lt;li&gt;This text """)
+    }
+    assertResult(true) {
+      outerXmlString.contains("""entities: &amp;lt;, &amp;gt;, &amp;amp;""")
+    }
+    assertResult(true) {
+      outerXmlString.contains(s"""xmlns="$innerNs"""")
+    }
+
+    // Let's deserialize and compare the results with the original
+
+    val outerXmlElem2 =
+      docParser.parse(new InputSource(new StringReader(outerXmlString))).documentElement
+
+    def emptyItemElem(e: simple.Elem): simple.Elem = {
+      if (e.localName == "item") e.withChildren(Vector()) else e
+    }
+
+    assertResult(resolved.Elem(outerXmlElem.transformElems(emptyItemElem)).removeAllInterElementWhitespace) {
+      resolved.Elem(outerXmlElem2.transformElems(emptyItemElem).removeAllInterElementWhitespace)
+    }
+
+    val itemElem = outerXmlElem2.findElem(_.localName == "item").get
+
+    val innerXmlElem2 =
+      docParser.parse(new InputSource(new StringReader(itemElem.text))).documentElement
+
+    assertResult(List(text1, text2)) {
+      innerXmlElem.filterElems(_.localName == "li").map(_.text)
+    }
+
+    assertResult(resolved.Elem(innerXmlElem).removeAllInterElementWhitespace) {
+      resolved.Elem(innerXmlElem2).removeAllInterElementWhitespace
+    }
+  }
 }
